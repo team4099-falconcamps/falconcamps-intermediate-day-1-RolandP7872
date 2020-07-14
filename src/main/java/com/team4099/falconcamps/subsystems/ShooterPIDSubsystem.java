f3cc2529098ad58409984a8b0c088c9a44203a8b
RolandP7872 <rolan0204@gmail.com>
2020-07-14 17:50:35 +0000

    I think i got it

--- src/main/java/com/team4099/falconcamps/subsystems/ShooterPIDSubsystem.java
+++ src/main/java/com/team4099/falconcamps/subsystems/ShooterPIDSubsystem.java
@@ -1,25 +1,26 @@
 package com.team4099.falconcamps.subsystems;
 
-import com.revrobotics.*;
+import com.revrobotics.CANEncoder;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel;
 
 import edu.wpi.first.wpilibj.controller.PIDController;
-import edu.wpi.first.wpilibj2.command.PIDCommand;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.PIDSubsystem;
 
-public class ShooterPIDSubsystem extends SubsystemBase {
-    private final static CANSparkMax shooterLeader = new CANSparkMax(1, CANSparkMaxLowLevel.MotorType.kBrushless);
-    private final static CANSparkMax shooterFollower = new CANSparkMax(14, CANSparkMaxLowLevel.MotorType.kBrushless);
-    private final static CANEncoder shooterEncoder = shooterLeader.getEncoder();
+public class ShooterPIDSubsystem extends PIDSubsystem {
+    private final  CANSparkMax shooterLeader = new CANSparkMax(1, CANSparkMaxLowLevel.MotorType.kBrushless);
+    private final  CANSparkMax shooterFollower = new CANSparkMax(14, CANSparkMaxLowLevel.MotorType.kBrushless);
+    private final  CANEncoder shooterEncoder = shooterLeader.getEncoder();
 
     public ShooterPIDSubsystem() {
-        super(new PIDController(1.7 / 300 , 8.0 , 5.5 * 14000));
-        double ShooterP = 1.7 / 300;
-        double ShooterI = 8.0;
-        double ShooterD = 5.5 * 14000;
+        super(new PIDController(1.7 / 300, 8.0, 5.5 * 14000));
         
     }
     public double getCurrentVelocity() {
         return shooterEncoder.getVelocity();
+    }
+    public double getMeasurement(){
+        return getCurrentVelocity();
     }
     public void useOutput(double setPoint, double output) {
         shooterLeader.set(output);
