b5b9829332de20e397c0a38a2bf511464721264a
RolandP7872 <rolan0204@gmail.com>
2020-07-15 18:23:26 +0000

    hmm

--- src/main/java/com/team4099/falconcamps/RobotContainer.java
+++ src/main/java/com/team4099/falconcamps/RobotContainer.java
@@ -6,7 +6,7 @@ public class RobotContainer {
     private final ShooterPIDSubsystem subsystem = new ShooterPIDSubsystem();
     XboxController driverController = new XboxController(0);
     public RobotContainer() {
-        new Trigger(driverController :: getAbutton).whileActive(new ShootSparkPIDCommand(subsystem));
-        intake.setDefaultCommand(new ShooterSparkPIDIdleCommand(intake));
+        new Trigger(driverController::getAButton).whileActive(new ShooterPIDSubsystem(subsystem));
+        subsystem.setDefaultCommand(new ShooterPIDSubsystemIdleCommand(subsystem));
     }
 }
