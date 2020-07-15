7dfcdbf8a7e6a6691c542aba08e880a5d1aeec36
RolandP7872 <rolan0204@gmail.com>
2020-07-15 17:49:28 +0000

    I think i got it

--- src/main/java/com/team4099/falconcamps/RobotContainer.java
+++ src/main/java/com/team4099/falconcamps/RobotContainer.java
@@ -3,8 +3,9 @@ package com.team4099.falconcamps;
 import com.team4099.falconcamps.subsystems.ShooterPIDSubsystem;
 
 public class RobotContainer {
+    private final ShooterPIDSubsystem subsystem = new ShooterPIDSubsystem();
+    XboxController driverController = new XboxController(0);
     public RobotContainer() {
-        final ShooterPIDSubsystem subsystem = new ShooterPIDSubsystem();
-
+        new Trigger(driverController :: getAbutton).whileActive(new ShootSparkPIDCommand(ShooterPIDSubsystem));
     }
 }
